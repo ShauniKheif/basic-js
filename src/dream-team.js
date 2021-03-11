80453eb1bde34459e3c229f2a1c3a04a58966928
ShauniKheif <53867436+ShauniKheif@users.noreply.github.com>
2021-03-11 16:35:41 +0300

    feat: add task solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) {
+    return false;
+  }
+
+  let dreamName = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == "string") {
+      members[i] = members[i].trim();
+      dreamName.push(members[i][0]);
+    } 
+    else if (typeof members[i] == "array") {
+      let arr = members[i];
+      for (let a = 0; a < arr.length; a++) {
+        arr[a] = arr[a].trim();
+        dreamName.push(arr[a][0]);
+      }
+    }
+  }
+  dreamName = dreamName.join("").toUpperCase();
+  dreamName = dreamName.split("").sort().join("");
+  return (dreamName);
+}
