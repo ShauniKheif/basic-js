8b83eb96476841ac0e29d9f381ac7b0f24aec5b3
ShauniKheif <53867436+ShauniKheif@users.noreply.github.com>
2021-03-11 15:51:27 +0300

    feat: add task solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,19 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (!sampleActivity) {
+    return false; 
+  }
+   else if (parseFloat(sampleActivity) <= 0 || parseFloat(sampleActivity) > MODERN_ACTIVITY) {
+    return false; 
+  }  
+  else if (!parseFloat(sampleActivity)) {
+    return false; 
+  } 
+
+  else if (typeof sampleActivity != "string") {
+    return false;
+  }
+
+  return Math.ceil((Math.log(MODERN_ACTIVITY/Number(sampleActivity))) / (0.693/HALF_LIFE_PERIOD)); 
 };
